36956f4c47ee25a610a34afada68fbc87748019a
Konrad Wasowicz <exaroth@gmail.com>
2019-02-25 18:23:59 +0000

    add setup.py make it pypi compatible

---
+++ README.md
@@ -0,0 +1,10 @@
+# Generic JSON parser for python3 dataclasses
+
+This is a heavily modified version of 
+[dataclasses-json](https://pypi.org/project/dataclasses-json/)
+module which allows for recursive encryption
+and decryption of JSON into common python3 dataclasses
+
+
+See https://github.com/lidatong/dataclasses-json/blob/master/README.md
+for usage details.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='dataclasses_json',
+    version='0.0.1',
+    packages=['parser'],
+    include_package_data=True,
+    description=(
+        "This module allows for easy conversion to and from JSON",
+        "format for common python3 dataclasses"
+    ),
+    long_description=open('README.txt').read(),
+    author='Ingresso',
+    author_email='systems@ingresso.co.uk',
+    install_requires=[
+        'python-dateutil==2.7.5',
+        'marshmallow==2.18.1'
+    ],
+)
