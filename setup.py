741b97b8e139db38224be345829e7c0094eb9d47
Konrad Wasowicz <exaroth@gmail.com>
2019-03-08 16:03:59 +0000

    make desc a string to hopefully fix ci issues

--- setup.py
+++ setup.py
@@ -6,13 +6,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dataclasses_json',
-    version='0.0.1',
+    version='1.0.1',
     packages=['dataclasses_json'],
     include_package_data=True,
-    description=(
-        "This module allows for easy conversion to and from JSON",
-        "format for common python3 dataclasses"
-    ),
+    description="Allows for easy dataclass parsing from/to JSON",
     long_description=open('README.md').read(),
     author='Ingresso',
     author_email='systems@ingresso.co.uk',
