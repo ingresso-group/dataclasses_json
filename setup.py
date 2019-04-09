7d8df16c5fa5fae231f07245f8d7e3a7d1e61370
Konrad Wasowicz <exaroth@gmail.com>
2019-04-09 17:54:42 +0100

    Update datetutil to 2.8.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dataclasses_json',
-    version='1.0.5',
+    version='1.0.6',
     packages=['dataclasses_json'],
     include_package_data=True,
     description="Allows for easy dataclass parsing from/to JSON",
@@ -14,7 +14,7 @@ setup(
     author='Ingresso',
     author_email='systems@ingresso.co.uk',
     install_requires=[
-        'python-dateutil==2.7.5',
+        'python-dateutil==2.8.0',
         'marshmallow==2.18.1'
     ],
 )
